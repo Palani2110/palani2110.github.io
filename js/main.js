64829bca71827a3161d484eb7cec76e800ec88b3
Palani2110 <142401042+Palani2110@users.noreply.github.com>
2025-02-22 09:39:23 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Palanikumar.", "Cloud Architect.", "DevOps Engineer."],
       typeSpeed: 100,
       loop: true,
     });
